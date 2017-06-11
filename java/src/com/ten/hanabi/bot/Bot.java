37c38c17a7738e0183be77f13a118484f0a4653b
Thomas BESSOU <thomas.bessou@hotmail.fr>
2017-06-11 17:48:57 +0200

    minor fix unused import

--- java/src/com/ten/hanabi/bot/Bot.java
+++ java/src/com/ten/hanabi/bot/Bot.java
@@ -2,7 +2,6 @@ package com.ten.hanabi.bot;
 
 import com.ten.hanabi.core.*;
 import com.ten.hanabi.core.exceptions.InvalidPlayException;
-import com.ten.hanabi.play.PlayManager;
 import com.ten.hanabi.play.PlayingEntity;
 
 public abstract class Bot extends PlayingEntity {
