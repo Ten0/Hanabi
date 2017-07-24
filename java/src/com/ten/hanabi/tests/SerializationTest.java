7979247658e169265cf750be9c0154c3840b68cb
Thomas BESSOU <thomas.bessou@telecom-paristech.fr>
2017-07-24 13:29:52 +0200

    SerializationTest is no longer scala-dependant

--- java/src/com/ten/hanabi/tests/SerializationTest.java
+++ java/src/com/ten/hanabi/tests/SerializationTest.java
@@ -1,6 +1,5 @@
 package com.ten.hanabi.tests;
 
-import com.flecheck.hanabi.bga.BGA;
 import com.ten.hanabi.core.Hanabi;
 import com.ten.hanabi.serialization.XMLSerializer;
 import com.ten.hanabi.ui.play.PlayFrame;
@@ -9,12 +8,6 @@ import com.ten.hanabi.ui.play.UIPlayManager;
 public class SerializationTest {
 
 	public static void main(String[] args) throws Exception {
-		/*
-		 * Hanabi h = BGA.getGameById(28441973);
-		 * 
-		 * XMLSerializer.toXML(h, "../games/G1.hnb");
-		 */
-
 		Hanabi h = XMLSerializer.loadHanabi("../games/G1.hnb");
 
 		UIPlayManager upm = new UIPlayManager();
